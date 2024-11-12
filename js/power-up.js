4749420900375338ff14468d67513c5f75538af1
mnesseroff <136466059+mnesseroff@users.noreply.github.com>
2024-11-12 10:40:55 +0000

    Update power-up.js

--- js/power-up.js
+++ js/power-up.js
@@ -2,14 +2,30 @@ window.TrelloPowerUp.initialize({
   'card-buttons': function(t, options) {
     return [{
       icon: {
-        dark: '../images/icon-light.png',
-        light: '../images/icon-dark.png'
+        dark: './images/icon-light.png',
+        light: './images/icon-dark.png'
       },
       text: 'Create Request',
       callback: function(t) {
         return t.modal({
           url: './form.html',
-          height: 600,
+          height: 700,
+          title: 'Creative Request Form'
+        });
+      }
+    }];
+  },
+  'board-buttons': function(t, options) {
+    return [{
+      icon: {
+        dark: './images/icon-light.png',
+        light: './images/icon-dark.png'
+      },
+      text: 'New Creative Request',
+      callback: function(t) {
+        return t.modal({
+          url: './form.html',
+          height: 700,
           title: 'Creative Request Form'
         });
       }
